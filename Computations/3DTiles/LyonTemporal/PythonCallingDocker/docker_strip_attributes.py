72ecaddd476519e4fc79e3979f54cd8a15cccaeb
jailln <vincentjaillot@gmail.com>
2020-07-08 17:03:44 +0200

    Update working directory and command of docker_strip_attributes

--- Computations/3DTiles/LyonTemporal/PythonCallingDocker/docker_strip_attributes.py
+++ Computations/3DTiles/LyonTemporal/PythonCallingDocker/docker_strip_attributes.py
@@ -7,16 +7,15 @@ from docker_helper import DockerHelper
 class DockerStripAttributes(DockerHelper):
 
     def __init__(self):
-        context_dir = os.pah.join(os.getcwd(),
-                                  '..',
-                                  'Docker',
-                                  'CityGML2Stripper-DockerContext')
+        context_dir = os.path.join(os.getcwd(),
+                                   '..',
+                                   'Docker',
+                                   'CityGML2Stripper-DockerContext')
         tag_name = 'liris:CityGML2Stripper'
         super().__init__(context_dir, tag_name)
-
+        self.working_dir = '/root/UD-Serv/Utils/CityGML2Stripper'
         self.input_filename = None
         self.output_filename = None
-        self.command_output_directory = None
 
     def assert_ready_for_run(self):
         if not self.input_filename:
@@ -37,17 +36,12 @@ class DockerStripAttributes(DockerHelper):
     def set_output_filename(self, output_filename):
         self.output_filename = output_filename
 
-    def set_command_output_directory(self, output_directory):
-        # This is internal to the container and as seen by the container
-        # command (as opposed to the directory mounted from "outside" the
-        # container i.e. DockerHelper.mounted_output_dir)
-        self.command_output_directory = output_directory
-
     def get_command(self):
         self.assert_ready_for_run()
-        command = 'python /src/CityGML2Stripper.py ' # Mind the trailing separator
-        command += '--input /Input/' + self.input_filename + ' '
-        command += '--output /Output/' + self.output_filename + ' ' #FIXME: peut etre qu'il faut concaténer output avec le command_output_directory
+        # We don't need to specify the executable since an entrypoint is specified in the DockerFile of
+        # DockerStripAttributes
+        command = '--input /Input/' + self.input_filename + ' '
+        command += '--output /Output/' + self.output_filename + ' '
         return command
 
 
@@ -60,26 +54,16 @@ if __name__ == '__main__':
                         filemode='w')
 
     d = DockerStripAttributes()
-    d.set_mounted_input_directory(os.path.join(os.getcwd(),
-                                               'junk/LYON_1ER_2009'))
 
-    # Setting up, the output directory:
-    #  1. side by side with input file: use
-    #     d.set_mounted_output_directory(d.get_mounted_input_directory())
-    #  2. in some arbitrary designated place: use e.g.
-    #     output_dir = os.path.join(os.getcwd(),'junk_strip')
-    #     if not os.path.exists(output_dir):
-    #         os.makedirs(output_dir)
-    #     d.set_mounted_output_directory(output_dir)
-    #  3. in the invocation directory: just leave the output directory unset
-    #
-    # Optionally one can define the output directory to be a sub-directory
-    # of the mounted output directory with
-    # d.set_command_output_directory('LYON_1ER')
-    #
-    # For this __main__, we leave it unset and thus expect the strip file
-    # in the invocation dir.
+    input_directory = os.path.join(os.getcwd(), 'junk/LYON_1ER_2009')
+    d.set_mounted_input_directory(input_directory)
     d.set_input_filename('LYON_1ER_BATI_2009_splited.gml')
     d.set_output_filename('LYON_1ER_BATI_2009_splited_stripped.gml')
 
     d.run()
+
+    # Since CityGML2Stripper does not allow to specify an output folder,
+    # you can use os.replace to move the resulting file to a specific folder.
+    # For instance, you can do:
+    #   os.replace('LYON_1ER_BATI_2009_splited_stripped.gml',
+    #              '/path/to/destination/folder/LYON_1ER_BATI_2009_splited_stripped.gml')
