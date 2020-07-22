046af197923a079247b92360842261f4fa2437d0
EricBoix <eric.boix@insa-lyon.fr>
2020-07-22 17:34:32 +0200

    Creation of a StripInputOutputs class
    The purpose of this class is to collect all the "knowledge"
    about the demo files chosen layout of the strip algorithm
    Original code located in docker_strip_attributes thus relocated
    in that class.

--- Computations/3DTiles/LyonTemporal/PythonCallingDocker/demo_configuration.py
+++ Computations/3DTiles/LyonTemporal/PythonCallingDocker/demo_configuration.py
@@ -1,11 +1,13 @@
 output_dir = 'junk'
 vintages = [2009, 2012, 2015]
-boroughs = ['LYON_1ER',
+boroughs = [
+           # 'LYON_1ER',
             'LYON_2EME',
             'LYON_3EME',
-            'LYON_4EME',
-            'LYON_5EME',
-            'LYON_6EME',
-            'LYON_7EME',
-            'LYON_8EME',
-            'LYON_9EME']
+           # 'LYON_4EME',
+           # 'LYON_5EME',
+           # 'LYON_6EME',
+           # 'LYON_7EME',
+           # 'LYON_8EME',
+           # 'LYON_9EME'
+]
