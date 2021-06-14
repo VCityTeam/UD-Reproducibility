1d213d891d0c770a0d75394c899c33fda55ff297
LorenzoMarnat <lmarnat14@gmail.com>
2021-06-14 17:26:04 +0200

    Six eyes are watching :eyes:

---
+++ Computations/3DTiles/ChemistryValley/DockerContext/demo_configuration_static.py
@@ -0,0 +1,16 @@
+ # This is an example of configuration file that must be customized to
+ # suit your specific needs.
+output_dir = 'junk'
+vintage = 2015
+city = 'CHEMISTRY_VALLEY'
+boroughs = [
+           'LYON_1ER',
+]
+ 
+database = {
+  'PG_HOST': '172.30.80.1',
+  'PG_PORT': '5435',
+  'PG_NAME': 'citydb-lyon-2009-for_static',
+  'PG_USER': 'postgres',
+  'PG_PASSWORD': 'postgres'
+}
