89225d25b9d036d3ece417589c2eaffd598aff29
EricBoix <eric.boix@insa-lyon.fr>
2021-12-02 11:16:20 +0100

    Removed the old version of PythonCalling Docker.
    The Expedata content is not located in https://github.com/VCityTeam/ExpeData-Workflows_testing

--- Computations/3DTiles/LyonTemporal/ArgoWorkflows/Docker/Collect-DockerContext/Tests/Readme.md
+++
@@ -1,3 +0,0 @@
-This directory is not used by the container. It's just a playground for testing
-some python gists.
-
--- Computations/3DTiles/LyonTemporal/ArgoWorkflows/Docker/Collect-DockerContext/Tests/import_yaml.py
+++
@@ -1,8 +0,0 @@
-import yaml
-
-with open('../../../demo_configuration_static.yaml') as stream:
-  try:
-    parsed_yaml=yaml.safe_load(stream)
-    print(parsed_yaml)
-  except yaml.YAMLError as e:
-    print(e)
