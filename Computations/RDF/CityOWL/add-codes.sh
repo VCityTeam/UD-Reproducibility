2ad7a7364e90cda774f24992538b3ccf068a85a6
Diego Vinasco-Alvarez <diego.vinasco@protonmail.com>
2024-02-05 17:44:50 +0100

    update scripts

--- Computations/RDF/CityOWL/add-codes.sh
+++ Computations/RDF/CityOWL/add-codes.sh
@@ -1,31 +1,31 @@
 #!/bin/sh
 
-cp ./stage-2/* ./stage-3/
+mkdir ./stage-3
 echo "Adding bridge-codes.ttl"
-python3 add_triples.py ./input/bridge.ttl ./concept-schemes/bridge-codes.ttl ./input/bridge.ttl
+python3 add_triples.py ./stage-2/bridge.ttl ./concept-schemes/bridge-codes.ttl ./stage-3/bridge.ttl
 echo "Adding building-codes.ttl"
-python3 add_triples.py ./input/building.ttl ./concept-schemes/building-codes.ttl ./input/building.ttl
+python3 add_triples.py ./stage-2/building.ttl ./concept-schemes/building-codes.ttl ./stage-3/building.ttl
 echo "Adding construction-codes.ttl"
-python3 add_triples.py ./input/construction.ttl ./concept-schemes/construction-codes.ttl ./input/construction.ttl
+python3 add_triples.py ./stage-2/construction.ttl ./concept-schemes/construction-codes.ttl ./stage-3/construction.ttl
 echo "Adding core-codes.ttl"
-python3 add_triples.py ./input/core.ttl ./concept-schemes/core-codes.ttl ./input/core.ttl
+python3 add_triples.py ./stage-2/core.ttl ./concept-schemes/core-codes.ttl ./stage-3/core.ttl
 echo "Adding document-codes.ttl"
-python3 add_triples.py ./input/document.ttl ./concept-schemes/document-codes.ttl ./input/document.ttl
+python3 add_triples.py ./stage-2/document.ttl ./concept-schemes/document-codes.ttl ./stage-3/document.ttl
 echo "Adding dynamizer-codes.ttl"
-python3 add_triples.py ./input/dynamizer.ttl ./concept-schemes/dynamizer-codes.ttl ./input/dynamizer.ttl
+python3 add_triples.py ./stage-2/dynamizer.ttl ./concept-schemes/dynamizer-codes.ttl ./stage-3/dynamizer.ttl
 echo "Adding furniture-codes.ttl"
-python3 add_triples.py ./input/cityfurniture.ttl ./concept-schemes/furniture-codes.ttl ./input/cityfurniture.ttl
+python3 add_triples.py ./stage-2/cityfurniture.ttl ./concept-schemes/furniture-codes.ttl ./stage-3/cityfurniture.ttl
 echo "Adding generics-codes.ttl"
-python3 add_triples.py ./input/generics.ttl ./concept-schemes/generics-codes.ttl ./input/generics.ttl
+python3 add_triples.py ./stage-2/generics.ttl ./concept-schemes/generics-codes.ttl ./stage-3/generics.ttl
 echo "Adding group-codes.ttl"
-python3 add_triples.py ./input/cityobjectgroup.ttl ./concept-schemes/group-codes.ttl ./input/cityobjectgroup.ttl
+python3 add_triples.py ./stage-2/cityobjectgroup.ttl ./concept-schemes/group-codes.ttl ./stage-3/cityobjectgroup.ttl
 echo "Adding landuse-codes.ttl"
-python3 add_triples.py ./input/landuse.ttl ./concept-schemes/landuse-codes.ttl ./input/landuse.ttl
+python3 add_triples.py ./stage-2/landuse.ttl ./concept-schemes/landuse-codes.ttl ./stage-3/landuse.ttl
 echo "Adding transportation-codes.ttl"
-python3 add_triples.py ./input/transportation.ttl ./concept-schemes/transportation-codes.ttl ./input/transportation.ttl
+python3 add_triples.py ./stage-2/transportation.ttl ./concept-schemes/transportation-codes.ttl ./stage-3/transportation.ttl
 echo "Adding tunnel-codes.ttl"
-python3 add_triples.py ./input/tunnel.ttl ./concept-schemes/tunnel-codes.ttl ./input/tunnel.ttl
+python3 add_triples.py ./stage-2/tunnel.ttl ./concept-schemes/tunnel-codes.ttl ./stage-3/tunnel.ttl
 echo "Adding vegetation-codes.ttl"
-python3 add_triples.py ./input/vegetation.ttl ./concept-schemes/vegetation-codes.ttl ./input/vegetation.ttl
+python3 add_triples.py ./stage-2/vegetation.ttl ./concept-schemes/vegetation-codes.ttl ./stage-3/vegetation.ttl
 echo "Adding waterbody-codes.ttl"
-python3 add_triples.py ./input/waterbody.ttl ./concept-schemes/waterbody-codes.ttl ./input/waterbody.ttl
+python3 add_triples.py ./stage-2/waterbody.ttl ./concept-schemes/waterbody-codes.ttl ./stage-3/waterbody.ttl
